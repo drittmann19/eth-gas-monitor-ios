bdaa43ce8011d985c4ba655d0e51bb0c01269142
Damean Rittmann <drittmann19@gmail.com>
2026-01-26 20:10:31 -0600

    Initial Commit

---
+++ EthGasMonitor/EthGasMonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthGasMonitorApp.swift
+//  EthGasMonitor
+//
+//  Created by Damean Rittmann on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct EthGasMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EthGasMonitorTests/EthGasMonitorTests.swift
@@ -0,0 +1,17 @@
+//
+//  EthGasMonitorTests.swift
+//  EthGasMonitorTests
+//
+//  Created by Damean Rittmann on 1/26/26.
+//
+
+import Testing
+@testable import EthGasMonitor
+
+struct EthGasMonitorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
